2b00602d29f20a8439ad06606f3e8e6f7e253d25
tpcamacho <148915931+tpcamacho@users.noreply.github.com>
2023-11-28 14:15:19 -0800

    pew

--- Game_Project_Group30/Assets/Scripts/PlayerController.cs
+++ Game_Project_Group30/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //returns true for every frame that the player is pressing to the D key
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("THe D key is being pressed");
+            //move the object over to the right with Vector3.right by speed (m/s)
+            //multiply that by Time.deltaTime to convert m/frame to m/s
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            Restrictions();
+        }
+
+        //returns true for every frame that the player is pressing to the D key
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Debug.Log("THe D key is being pressed");
+            //move the object over to the left with Vector3.left by speed (m/s)
+            //multiply that by Time.deltaTime to convert m/frame to m/s
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            Restrictions();
+        }
+    }
+
+    /// <summary>
+    /// Prevents bucket from going off screen
+    /// </summary>
+    private void Restrictions()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y, transform.position.z);
     }
 }
