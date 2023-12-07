5ee6d7eca3e85b3f1bdfec7e8bb1a879c9f67d58
tpcamacho <148915931+tpcamacho@users.noreply.github.com>
2023-12-07 13:16:57 -0800

    idk

--- Game_Project_Group30/Assets/Scripts/ShipMovement.cs
+++ Game_Project_Group30/Assets/Scripts/ShipMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5;
+    private float dist;
+    private float distMin = -5f;
+    private float disMax = 5f;
+    private Vector3 temp;
+
+    public bool goingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Move();
     }
 
     // Update is called once per frame
@@ -18,11 +24,42 @@ public class ShipMovement : MonoBehaviour
         transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 
+    private void Move()
+    {
+        if (goingRight)
+        {
+            if (transform.position.x >= -dist)
+            {
+                temp = Vector3.left;
+                SetRandomDirectionSwitch();
+                goingRight = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x <= dist)
+            {
+                temp = Vector3.right;
+                SetRandomDirectionSwitch();
+                goingRight = true;
+            }
+        }
+        transform.position += temp * Time.deltaTime * speed;
+    }
+
+    private void SetRandomDirectionSwitch()
+    {
+        dist = Random.Range(distMin, disMax);
+    }
+
+    /*
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Boundary")
         {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.002f, transform.position.z);
             speed *= -1;
         }
     }
+    */
 }
